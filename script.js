adc3351b4617256a3afaf43c32e75fe941ab5371
theDAOS <sanjusanthosh46@gmail.com>
2025-03-23 14:08:20 +0530

    displayFeedback

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const baseURL = 'https://ftsdm04s2-2512-evaluation-default-rtdb.asia-southeast1.firebasedatabase.app/feedback';
 
-document.body.onload = loadData;
+document.body.onload = () => {
+    navigation();
+    loadData();
+};
 
 document.getElementById('feedbackForm').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -13,7 +16,7 @@ document.getElementById('feedbackForm').addEventListener('submit', (event) => {
     const feedbackMessage = form['feedbackMessage'].value;
     const isAnonymous = form['anonymous'].checked;
 
-    if ((name === "" || email === "") && !isAnonymous ) {
+    if ((name === "" || email === "") && !isAnonymous) {
         alert('Invalid Input!');
         return;
     }
@@ -27,11 +30,73 @@ document.getElementById('feedbackForm').addEventListener('submit', (event) => {
         'data': new Date,
     }
 
-    console.log(data);
+    // console.log(data);
 
     saveFeedback(data);
+    loadData();
     form.reset();
 })
+
+function displayFeedback() {
+    let data = localStorage.getItem('feedback') || '[]';
+    data = JSON.parse(data);
+    data = Object.entries(data).map(([id, value]) => {
+        return { ...value };
+    });
+
+    // console.log(data);
+
+    const section = document.getElementById('ViewFeedbackSection');
+    section.innerHTML = ``;
+
+    data.forEach(row => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const name = document.createElement('h3');
+        name.innerText = "Name: " + ((row.isAnonymous) ? 'Anonymous' : row.name);
+        card.appendChild(name);
+
+        const submittedOn = document.createElement('i');
+        submittedOn.innerText = new Date(row.data);
+        submittedOn.style.fontWeight = 'lighter';
+        card.appendChild(submittedOn);
+
+        const email = document.createElement('p');
+        email.innerText = "Email: " + ((row.isAnonymous) ? 'Anonymous' : row.email)
+        card.appendChild(email);
+
+        const rating = document.createElement('p');
+        rating.innerText = "Rating: "+ row.rating;
+        card.appendChild(rating);
+
+        const feedback = document.createElement('p');
+        feedback.innerText = "feedback: \n"+ row.feedbackMessage;
+        card.appendChild(feedback);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete Feedback';
+        deleteButton.onclick = () => {
+            deleteFeedback(row.id);
+        }
+        card.appendChild(deleteButton);
+
+        section.appendChild(card);
+    });
+}
+
+async function deleteFeedback(id) {
+    try {
+        const response = await fetch (baseURL+'/'+id+'.json', {
+            'method': 'DELETE',
+        })
+        const data = await response.json();
+        console.log(data);
+        loadData();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 async function saveFeedback(obj) {
     try {
@@ -41,23 +106,23 @@ async function saveFeedback(obj) {
             'body': JSON.stringify(obj),
         });
         const data = await response.json();
+        alert('Feedback accepted...');
         console.log(data);
     } catch (error) {
+        alert('Filed to send Feedback!!!');
         console.log(error);
     }
 }
 
-
 async function loadData() {
-    navigation();
     try {
         const response = await fetch(baseURL + '.json');
         const data = await response.json();
-    
+
         console.log(data);
 
         feedbackData = Object.entries(data).map(([id, value]) => {
-            return {id, ...value}
+            return { id, ...value }
         })
 
         localStorage.setItem('feedback', JSON.stringify(feedbackData));
@@ -66,7 +131,6 @@ async function loadData() {
         console.log(error);
     }
 }
-
 
 function navigation(option = 'home') {
     const home = document.getElementById('HomeSection');
@@ -80,14 +144,18 @@ function navigation(option = 'home') {
     switch (option) {
         case 'home':
             home.style.display = '';
+            document.title = "Home";
             break;
 
         case 'submitForm':
             submitForm.style.display = '';
+            document.title = "Submit Your Feedback";
             break;
 
         case 'feedback':
             feedback.style.display = '';
+            document.title = "Student Feedback Entries";
+            displayFeedback();
             break;
     }
 }
